8baa21f303bb9c9a4cefa46b7415a2e401e562ff
payaneco <payaneco@gmail.com>
2020-01-05 19:59:40 +0900

    Change paths.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 apt-get install -y fonts-ipaexfont
 
 cp /home/pi/TearOff/startup_calendar.sh /usr/local/bin/
-set -i -e "s/exit 0/startup_calendar.sh/\n\nexit 0/g" /etc/rc.local
+set -i -e "s/exit 0/startup_calendar.sh\n\nexit 0/g" /etc/rc.local
 
 python3 /home/pi/TearOff/auto_calendar.py

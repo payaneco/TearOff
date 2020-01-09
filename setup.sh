90262f03f630d189c8c7b2c6aa5115c128c51d0a
payaneco <payaneco@gmail.com>
2020-01-09 20:47:43 +0900

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 apt-get install -y fonts-ipaexfont
 
-#cp /home/pi/TearOff/startup_calendar.sh /usr/local/bin/
-sed -i -e "s/exit 0\$/\/home\/pi\/TearOff\/startup_calendar.sh\n\nexit 0/g" /etc/rc.local
+if [ ! "`cat /etc/rc.local | grep startup_calendar.sh`" ]; then
+    # cp /home/pi/TearOff/startup_calendar.sh /usr/local/bin/
+    sed -i -e "s/^exit 0\$/\/home\/pi\/TearOff\/startup_calendar.sh\n\nexit 0/g" /etc/rc.local
+fi
 
 python3 /home/pi/TearOff/auto_calendar.py
